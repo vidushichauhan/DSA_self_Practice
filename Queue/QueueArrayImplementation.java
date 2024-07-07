2e91a6b4e67ed97614e3ed22078154486abc8cd0
Vidushi Chauhan <vidushic0@gmail.com>
2024-07-07 12:24:18 -0400

    Implement Queue using array

---
+++ Queue/QueueArrayImplementation.java
@@ -0,0 +1,86 @@
+package Queue;
+
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class GfG
+{
+	public static void main(String args[])
+	{
+		 Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 {
+			MyQueue obj = new MyQueue();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+
+
+class MyQueue {
+
+    int front, rear;
+	int arr[] = new int[100005];
+
+    MyQueue()
+	{
+		front=0;
+		rear=-1;
+	}
+	
+	//Function to push an element x in a queue.
+	void push(int x)
+	{   if (rear < arr.length - 1) { // Check to prevent ArrayIndexOutOfBoundsException
+            arr[++rear] = x; // Increment rear then insert the element
+        }
+	    
+	} 
+
+    //Function to pop an element from queue and return that element.
+	int pop()
+	{   if (isEmpty()) { // Check if queue is empty
+            return -1;
+        }
+        int res = arr[front]; // Get the front element
+        front++; // Move front forward instead of shifting elements
+        if (front > rear) { // Reset front and rear if the queue becomes empty
+            front = 0;
+            rear = -1;
+        }
+        return res;
+	} 
+	
+	boolean isEmpty() {
+        return rear < front;
+    }
+}
+
+
+
+
